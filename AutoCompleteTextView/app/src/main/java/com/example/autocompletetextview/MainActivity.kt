52c39b275b1a503280116eaf2d27f7c897fbee7b
Gil Campos <47683202+Gil-Campos@users.noreply.github.com>
2024-01-23 21:57:27 -0600

    Finish example on how to work with the AutoCompleteTextView and customize ite

--- AutoCompleteTextView/app/src/main/java/com/example/autocompletetextview/MainActivity.kt
+++ AutoCompleteTextView/app/src/main/java/com/example/autocompletetextview/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.autocompletetextview
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.autocompletetextview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpAutoCompleteTextView()
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun setUpAutoCompleteTextView() {
+        val stringArr = resources.getStringArray(R.array.languages)
+
+        val arrayAdapter = ArrayAdapter(this, R.layout.drop_down_item, stringArr)
+
+        binding.autoCompleteTextView.setDropDownBackgroundDrawable(this.getDrawable(R.drawable.no_padding_dropdown))
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
     }
 }
